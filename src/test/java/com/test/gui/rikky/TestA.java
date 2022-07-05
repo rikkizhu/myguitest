253cb8c0bb3edab27e72b9cbfdeaa5e466b64386
Rikky <zhuruiqi1994@gmail.com>
2022-07-06 00:05:46 +0800

    测试用demo

---
+++ src/test/java/com/test/gui/rikky/TestA.java
@@ -0,0 +1,36 @@
+package com.test.gui.rikky;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+/**
+ * @program: com.test.gui.rikky.TestA
+ * @description:
+ * @author: zhuruiqi
+ * @create: 2022-07-05 23:15
+ **/
+public class TestA {
+
+    @Test
+    public void test1() {
+        ChromeDriver chromeDriver = new ChromeDriver();
+        chromeDriver.navigate().to("https://weather.cma.cn/web/weather/58367.html");
+        String title = chromeDriver.getTitle();
+        assert(title.contains("气象局"));
+        chromeDriver.quit();
+    }
+
+    @Test
+    public void test2() {
+        ChromeDriver chromeDriver = new ChromeDriver();
+        chromeDriver.navigate().to("https://weather.cma.cn/web/weather/58367.html");
+        String title = chromeDriver.getTitle();
+        assert(title.contains("中国"));
+        chromeDriver.quit();
+    }
+
+
+
+
+
+}
